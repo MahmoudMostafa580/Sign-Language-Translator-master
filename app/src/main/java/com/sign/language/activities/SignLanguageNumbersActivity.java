22d5b58167887072328dd2094ec6aeafae06a7d2
MahmoudMostafa580 <mahmoud.mostafa20112011@gmail.com>
2021-08-18 01:41:26 +0200

    changeAlphabetsPhotos

---
+++ app/src/main/java/com/sign/language/activities/SignLanguageNumbersActivity.java
@@ -0,0 +1,16 @@
+package com.sign.language.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sign.language.R;
+
+public class SignLanguageNumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_language_numbers);
+    }
+}
