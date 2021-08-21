2a52078cdb7f35ecab655080f6d1e470512645db
MahmoudMostafa580 <mahmoud.mostafa20112011@gmail.com>
2021-08-21 14:14:14 +0200

    addLauncherActivity

---
+++ app/src/main/java/com/sign/language/activities/LauncherActivity.java
@@ -0,0 +1,29 @@
+package com.sign.language.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.WindowManager;
+
+import com.sign.language.R;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(),SearchTextActivity.class));
+            finish();
+        },2000);
+    }
+}
